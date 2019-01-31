d0d3d0815ed300c89c1cb2be7a4ca808bf23def4
TeamHiddenSam <teamhiddensam@gmail.com>
2019-01-31 09:56:08 +0000

    Started UI

---
+++ Rise of Xeru Unity/Assets/Scenes/ClassSelection.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClassSelection : MonoBehaviour {
+
+    bool confessor = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ConfessorToggle()
+    {
+        confessor = !confessor;
+    }
+}
